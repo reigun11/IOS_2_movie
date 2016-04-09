395517cbcde1be01d636259b0de10e0add42fffc
Trainning Educational <Trainning@macbook-de-trainning-10.local>
2016-04-09 17:45:53 -0300

    inclusão do alert

--- MovieList/Movie.h
+++ MovieList/Movie.h
@@ -16,4 +16,6 @@
 
 - (NSString *)stringRepresentation;
 
+
+
 @end
--- MovieList/MovieListViewController.m
+++ MovieList/MovieListViewController.m
@@ -43,6 +43,21 @@
         [_movies addObject:movie];
         [self updateUI];
     }
+    else{
+        NSString *title =@"Informacoes invalidadas";
+        NSString *msg =@"Digite os coisa campos corretamentes";
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title  message:msg preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"ok" style:UIAlertActionStyleDefault  handler:^(UIAlertAction * _Nonnull action) {
+            //NSLog(@"errou manow");
+            [_movieNameTextField becomeFirstResponder];
+        }];
+        [alertController addAction:action];
+        [self presentViewController:alertController
+                           animated:YES
+                         completion:nil];
+         
+    }
+
 }
 
 - (void) updateUI{
